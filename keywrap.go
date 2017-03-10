ffa3c374bfdf96d074c02df25c2845c2ea872fb1
Guillaume Leroi <guillaume-externe.leroi@erdf.fr>
2017-03-10 14:40:59 +0100

    add examples documenting how to wrap and unwrap

--- keywrap.go
+++ keywrap.go
@@ -1,7 +1,8 @@
 // Package keywrap provides a set of algorithms to wrap/unwrap keywrap with an encryption key.
 //
 // Currently, the following algorithms are implemented:
-//  - RFC3394: AES Key Wrap Algorithm
+//
+// - RFC3394: AES Key Wrap Algorithm
 package keywrap
 
 // KeyWrapper defines the methods to wrap and unwrap a key with an encryption key
---
+++ keywrap_test.go
@@ -0,0 +1,34 @@
+package keywrap_test
+
+import (
+	"encoding/hex"
+	"fmt"
+	"keywrap/rfc3394"
+	"strings"
+)
+
+func Example_wrap() {
+	var wrapper = rfc3394.NewWrapper()
+	kek, err := hex.DecodeString("000102030405060708090A0B0C0D0E0F")
+	key, err := hex.DecodeString("00112233445566778899AABBCCDDEEFF")
+	wrappedKey, err := wrapper.Wrap(kek, key)
+	if err != nil {
+		panic(err)
+	}
+	var hexWrappedKey = strings.ToUpper(hex.EncodeToString(wrappedKey))
+	fmt.Println(hexWrappedKey)
+	// Output: 1FA68B0A8112B447AEF34BD8FB5A7B829D3E862371D2CFE5
+}
+
+func Example_unwrap() {
+	var wrapper = rfc3394.NewWrapper()
+	kek, err := hex.DecodeString("000102030405060708090A0B0C0D0E0F")
+	wrappedKey, err := hex.DecodeString("1FA68B0A8112B447AEF34BD8FB5A7B829D3E862371D2CFE5")
+	key, err := wrapper.Unwrap(kek, wrappedKey)
+	if err != nil {
+		panic(err)
+	}
+	var hexKey = strings.ToUpper(hex.EncodeToString(key))
+	fmt.Println(hexKey)
+	// Output: 00112233445566778899AABBCCDDEEFF
+}
